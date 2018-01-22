d66984c1cc34e721751084cb04c684b52c8a8e79
harinder.chagger <harinder.chagger@dal.ca>
2018-01-22 16:22:12 -0400

    roger

--- task1.py
+++ task1.py
@@ -1,9 +1,27 @@
-import math
+from math import *
 
-class Point:
-    """Represents a point in 2-D space."""
+class point:
+      x = 0
+      y = 0
+
+
+def __init__(self,a,b):
+       self.x=a
+       self.y=b
+
+
+def distance_between_points(obj1,obj2):
+       distance_x=abs(obj1.x-obj2.x)
+       distance_y=abs(obj1.y-obj2.y)
+       distance = sqrt(distance_x**2+distance_y**2)
+       return (distance)
+
+
+object1 = point(20,40)
+object2 = point(40,60)
+print(distance_between_points(object1,object2))
 
 
 
-blank = Point()
+#blank = Point()
 
